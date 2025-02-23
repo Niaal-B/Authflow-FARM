d2f7734daefce218b4b551b3e381600451e21156
Niaal-B <nihnihal504@gmail.com>
2025-02-23 15:28:10 +0530

    feat: added update username route to admin

--- Backend/app/api/routes/admin.py
+++ Backend/app/api/routes/admin.py
@@ -9,5 +9,11 @@ admin_router = APIRouter()
 async def list_users(search: str = "", limit: int = 10, page: int = 1, admin=Depends(get_current_admin)):
     query = {"email": {"$regex": search, "$options": "i"}} if search else {}
     users = await db.user.find(query).skip((page - 1) * limit).limit(limit).to_list(length=limit)
-    
-    return [{"id": str(user["_id"]), "email": user["email"]} for user in users]
+    print(users)
+    return [{"id": str(user["_id"]),"name": user["name"] , "email": user["email"],} for user in users]
+
+
+@admin_router.put("/users/{user_id}")
+async def update_user(user_id: str,name: str,admin=Depends(get_current_admin)):
+    await db.user.update_one({"_id":ObjectId(user_id)},{"$set": {"name": name}})
+    return {"message":"User updated"}
